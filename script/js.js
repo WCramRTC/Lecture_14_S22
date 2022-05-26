b052db04b5ee896bb3a8b9df96160c3d6bf6710f
WCram <wcram@rtc.edu>
2022-05-26 11:25:57 -0700

    Final Update

---
+++ script/js.js
@@ -0,0 +1,13 @@
+const display = document.getElementsByClassName("display")[0];
+const userInput = document.getElementsByClassName("input");
+const btnDisplay = document.getElementById("btnDisplay");
+
+btnDisplay.addEventListener('click' , DisplayInfo);
+
+
+function DisplayInfo() {
+    for(let i = 0; i < userInput.length; i++) {
+        display.innerText += userInput[i].value + "\n";
+    }
+}
+
